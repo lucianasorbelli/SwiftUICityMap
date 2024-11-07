c87136ba05b92315e38c69dab6c28e024d720435
Luciana Sorbelli <luciana.campossorbelli@externos-cl.cencosud.com>
2024-11-06 23:59:28 -0300

    initial commit

---
+++ CityMapProject/CityMapProject/CityMapProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityMapProjectApp.swift
+//  CityMapProject
+//
+//  Created by Luciana Sorbelli on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CityMapProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityMapProject/CityMapProjectTests/CityMapProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityMapProjectTests.swift
+//  CityMapProjectTests
+//
+//  Created by Luciana Sorbelli on 06/11/2024.
+//
+
+import Testing
+@testable import CityMapProject
+
+struct CityMapProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
